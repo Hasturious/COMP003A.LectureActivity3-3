d3a42d0500acf9556672666b8f5484aa1c1a4fb5
Jean Bryant Figueroa <jbster559@gmail.com>
2025-01-27 12:30:27 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace COMP003A.LectureActivity3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Advanced Control Statements Demo!");
+            Console.Write("\nEnter a positive interger to iterate through: ")
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Would you like to skip a number? (yes/no): ");
+            //figure out how to bool here
+           
+        }
+    }
+}
